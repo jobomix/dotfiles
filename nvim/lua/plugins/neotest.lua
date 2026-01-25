4002f4e6404287d40ce90f6c54e5ed58eb7eeab4
Bruno Abitbol <brunoabitbol@bluelightcard.co.uk>
2026-01-25 08:53:25 +0000

    updated nvim plugins

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,55 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "marilari88/neotest-vitest",
+    "nvim-neotest/neotest-python",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("neotest").setup({
+      log_level = vim.log.levels.DEBUG,
+      adapters = {
+        require("neotest-vitest"),
+        require("neotest-python")({
+          args = { "-s" },
+        }),
+      },
+    })
+  end,
+
+  vim.keymap.set(
+    "n",
+    "<leader>tm",
+    ':lua require("neotest").run.run()<CR>',
+    { desc = "Run test [m]ethod above the cursor", noremap = true, silent = true }
+  ),
+  vim.keymap.set(
+    "n",
+    "<leader>to",
+    ':lua require("neotest").output.open()<CR>',
+    { desc = "Open test [o]utput", noremap = true, silent = true }
+  ),
+
+  vim.keymap.set(
+    "n",
+    "<leader>td",
+    ':lua require("neotest").run.run({strategy = "dap"})<CR>',
+    { desc = "[D]ebug test", noremap = true, silent = true }
+  ),
+
+  vim.keymap.set(
+    "n",
+    "<leader>tf",
+    ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
+    { desc = "Run all tests in [F]ile", noremap = true, silent = true }
+  ),
+  vim.keymap.set(
+    "n",
+    "<leader>ts",
+    ':lua require("neotest").stop()<CR>',
+    { desc = "Stop running tests", noremap = true, silent = true }
+  ),
+}
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,36 +4,102 @@ return {
   lazy = false,
   build = ":TSUpdate",
   event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<M-space>",
-        node_incremental = "<M-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { "ruby" },
-    },
-    indent = { enable = true, disable = { "ruby" } },
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
   },
-  config = function(_, opts)
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-    -- Prefer git instead of curl in order to improve connectivity in some environments
+  config = function()
     require("nvim-treesitter.install").prefer_git = true
     ---@diagnostic disable-next-line: missing-field
-    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<M-space>",
+          node_incremental = "<M-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { "ruby" },
+      },
+      indent = { enable = true, disable = { "ruby" } },
+
+      textobjects = {
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>na"] = "@parameter.inner",
+            ["<leader>nm"] = "@function.outer",
+          },
+          swap_previous = {
+            ["<leader>pa"] = "@parameter.inner",
+            ["<leader>pm"] = "@function.outer",
+          },
+        },
+
+        lsp_interop = {
+          enable = true,
+          border = "none",
+          floating_preview_opts = {},
+          peek_definition_code = {
+            ["<leader>df"] = "@function.outer",
+            ["<leader>dF"] = "@class.outer",
+          },
+        },
+
+        select = {
+          enable = true,
+
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ii"] = "@conditional.inner",
+            ["ai"] = "@conditional.outer",
+            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+            -- nvim_buf_set_keymap) which plugins like which-key display
+            ["<M-ic>"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            -- You can also use captures from other query groups like `locals.scm`
+            ["<M-as>"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+          },
+          -- You can choose the select mode (default is charwise 'v')
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * method: eg 'v' or 'o'
+          -- and should return the mode ('v', 'V', or '<c-v>') or a table
+          -- mapping query_strings to modes.
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+          -- If you set this to `true` (default is `false`) then any textobject is
+          -- extended to include preceding or succeeding whitespace. Succeeding
+          -- whitespace has priority in order to act similarly to eg the built-in
+          -- `ap`.
+          --
+          -- Can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * selection_mode: eg 'v'
+          -- and should return true or false
+          include_surrounding_whitespace = true,
+        },
+      },
+    })
 
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
